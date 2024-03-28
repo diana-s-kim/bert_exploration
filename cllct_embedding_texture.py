5cc47a5e8b3abaee20acf42c1e4924df9d8f3d10
Diana Kim <kimds@login510-27.ibex.kaust.edu.sa>
2024-03-28 12:55:17 +0300

    first commit

---
+++ cllct_embedding_texture.py
@@ -0,0 +1,94 @@
+#embedding layer access
+import numpy as np
+import torch
+import torch.nn as nn
+import pickle
+from Qformer import BertConfig, BertLMHeadModel
+from transformers import BertTokenizer
+from data.artemis import ArtEmis
+#from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+#model loading #simply
+device = torch.device("cuda:" + str(0) if torch.cuda.is_available() else "cpu")
+
+def init_Qformer(num_query_token=32, vision_width=1408, cross_attention_freq=2):
+        encoder_config = BertConfig.from_pretrained("bert-base-uncased")
+        encoder_config.encoder_width = vision_width
+        # insert cross-attention layer every other block
+        encoder_config.add_cross_attention = False
+        encoder_config.cross_attention_freq = cross_attention_freq
+        encoder_config.query_length = num_query_token
+        Qformer = BertLMHeadModel.from_pretrained("bert-base-uncased", config=encoder_config)
+        query_tokens = nn.Parameter(
+            torch.zeros(1, num_query_token, encoder_config.hidden_size)
+        )
+        query_tokens.data.normal_(mean=0.0, std=encoder_config.initializer_range)
+        return Qformer
+
+def init_tokenizer(truncation_side="right"):
+       tokenizer = BertTokenizer.from_pretrained("bert-base-uncased", truncation_side=truncation_side)
+       tokenizer.add_special_tokens({"bos_token": "[DEC]"})
+       return tokenizer
+
+class BERT_space(nn.Module):
+        def __init__(self,embed_dim=256):
+                super().__init__()
+                self.tokenizer = init_tokenizer()   
+                self.Qformer=init_Qformer()
+                self.Qformer.resize_token_embeddings(len(self.tokenizer))
+                self.text_proj = nn.Linear(self.Qformer.config.hidden_size, embed_dim)
+                self.color_texture_comp_head=nn.Linear(embed_dim,3)
+
+        def forward(self,X):
+                return self.Qformer(X)
+
+        def embedding(self,X):
+                texts = X["text"]
+                text = [text+" texture composition color " for text  in texts]
+                print("appended txt:",text)
+                print(text)
+                splited_txt=[self.tokenizer.tokenize(txt) for txt in text]
+                print(splited_txt[0])
+                text_tokens = self.tokenizer(text,truncation=True,max_length=512,return_tensors="pt").to(device)
+                text_tokens_=text_tokens["input_ids"]
+                attention_mask=text_tokens.attention_mask
+                print("input_id",text_tokens_)
+                print("input_txt",splited_txt)
+                text_output=self.Qformer(text_tokens_)#sequential
+                return splited_txt
+
+def collect(dataloader, device, model):
+        splited_txt=[]
+        for batch, X in enumerate(dataloader):
+                splited_txt.extend(model.embedding(X))
+        return splited_txt
+
+def main():
+        bert=BERT_space().to(device)
+#        activation={"last_hidden":np.empty((0,32,768))}#lst append
+        activation={"last_hidden":[]}
+        def getActivation(name):
+                def hook(model, input, output):
+#                    activation[name]=np.append(activation[name],output["last_hidden_state"].cpu().data.numpy(),axis=0)
+                    activation[name].append(output["last_hidden_state"].cpu().data.numpy())# different length
+                return hook
+        bert.Qformer.bert.register_forward_hook(getActivation('last_hidden'))
+
+        #wihtout
+        #dataloader
+        artemis=ArtEmis(data_csv="/ibex/ai/home/kimds/Research/P2/withLLM/making_it_works/data/data_after.csv",img_dir="/ibex/ai/home/kimds/Research/P2/withLLM/making_it_works/img_after/")
+        dataloader = DataLoader(artemis, batch_size=1, shuffle=False)
+        
+        #cllct
+        results=collect(dataloader, device, bert)
+        with open("./hidden_state/splited_txt_wo.txt","w") as f:
+                for item in results:
+                        f.write(" ".join(item)+"\n")
+        f.close()
+
+        ##filterin##
+        with open("./hidden_state/output_attn_prob_wo.pkl","wb") as f:
+                pickle.dump(activation,f)
+
+if __name__ == "__main__":
+    main()
---
+++ styles.py
@@ -0,0 +1,8 @@
+#style cllection to expriment
+
+ABSTRACT_STYLES = ['Abstract_Expressionism', 'Cubism','Color_Field_Painting', 'Minimalism', 'Action_painting', 'Synthetic_Cubism', 'Analytical_Cubism']
+EXTENDED_ABSTRACT_STYLES = ['Abstract_Expressionism', 'Cubism','Color_Field_Painting', 'Minimalism', 'Action_painting', 'Synthetic_Cubism', 'Analytical_Cubism','Fauvism','Pop_Art']
+
+
+
+
